f0a1beabcb5bc912a6f4c425d77ceedf71b25707
Julien Dufresne <julien@digitalmomentum.io>
2018-07-15 23:19:45 -0400

    Update TranslateModule

--- lib/translate.module.ts
+++ lib/translate.module.ts
@@ -1,17 +1,22 @@
-import { Module } from "@nestjs/common";
+import { DynamicModule, Global, Module } from "@nestjs/common";
 import { TranslateLoader } from "./loader/translate.loader";
 import { DefaultTranslationParser, TranslateParser } from "./parser/translate.parser";
 import { TranslateService } from "./translate.service";
 
-@Module({
-    providers: [
-        { provide: TranslateParser, useClass: DefaultTranslationParser },
-        TranslateLoader,
-        TranslateService
-    ],
-    exports: [
-        TranslateService
-    ]
-})
+@Global()
+@Module({})
 export class TranslateModule {
+    public static forRoot(prefix: string): DynamicModule {
+        return {
+            module: TranslateModule,
+            providers: [
+                { provide: TranslateParser, useClass: DefaultTranslationParser },
+                { provide: TranslateLoader, useValue: new TranslateLoader(prefix) },
+                TranslateService
+            ],
+            exports: [
+                TranslateService
+            ]
+        };
+    }
 }
