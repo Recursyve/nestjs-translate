5fcf20e64a803478f7c488d29dc780d8dd2dffc8
Julien Dufresne <julien@recursyve.io>
2019-07-22 23:12:40 -0400

    Export translation options

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,9 @@
 import { TranslateModule } from './translate.module';
 import { TranslateService } from './translate.service';
+import { TranslateOptionsModel } from './models/translate-options.model';
 
 export {
     TranslateModule,
     TranslateService,
+    TranslateOptionsModel
 };
