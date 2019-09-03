d0ec8c8cfa0b04bc51e3f38129084c8eaebbf0d4
Brandon Roberge <brandonroberge@hotmail.ca>
2019-09-03 14:16:36 -0400

    Add language list

--- lib/store/translate.store.ts
+++ lib/store/translate.store.ts
@@ -3,5 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class TranslateStore {
     public defaultLang: string;
+    public langs: string[] = [];
     public translations: { [lang: string]: unknown } = {};
 }
